c17752412edc509ebda2abe64bf18b1ebc010185
Jeyson Grisales <grisalesjeyson@gmail.com>
2024-09-04 21:16:33 -0500

    Iniciando proyecto

---
+++ cuyabros-cycling-team/src/component.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export default function component() {
+
+    
+  return (
+    <div>
+      
+    <input type="text" />
+    <button>Actualizar</button>
+    <p>Texto input</p>
+    <p>Texto Actualizado</p>
+
+    </div>
+  )
+}
